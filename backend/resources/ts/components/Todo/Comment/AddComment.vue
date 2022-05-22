722522897acca5b841e756202e7bb89579a9a900
watter46 <w.aoki.14@gmail.com>
2022-05-22 23:49:29 +0900

    Comment コンポーネントとモジュール作成

---
+++ backend/resources/ts/components/Todo/Comment/AddComment.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import type { CommentType, StateType, TaskType } from '../Functions/type'
+import { key as StateKey } from '../Store/InjectionKey/StateKey'
+import { inject, ref } from 'vue'
+
+
+interface Props {
+  titleIndex: number,
+  taskIndex: number
+}
+
+const props = defineProps<Props>();
+
+const commentInput = ref('')
+
+
+const state = inject(StateKey) as StateType
+
+
+const addComment = (input: string) => {
+  const addListObject: CommentType = {
+    id: state.maxCommentId + 1,
+    comment: input,
+  }
+
+  const taskList = state.testList[props.titleIndex].tasks as TaskType[]
+  const commentList = taskList[props.taskIndex].comments as CommentType[]
+
+  commentList.push(addListObject)
+
+  commentInput.value = ""
+}
+</script>
+
+<template>
+  <div class="addInput-flex my-5">
+    <input type="text" class="addInput-flex__input" v-model="commentInput" placeholder="commentを入力してください">
+    <div class="addInput-flex__add">
+      <span type="button" class="buttonCircle" @click="addComment(commentInput)">
+        <span class="buttonCircle-plus"></span>
+      </span>
+    </div>
+  </div>
+</template>
